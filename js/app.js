29198ccdc6ece323e06798cf1ab165035852c82b
Alfredo Eleazar Orozco Quesada <ae.orozco@uniandes.edu.co>
2024-06-19 17:33:38 -0500

    Acumulado

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ function butHoy(){
 	document.getElementById("hoy").style.display = 'block';
 	document.getElementById("actividad").style.display = 'none';
 	document.getElementById("acumulado").style.display = 'none';
+	document.getElementById("tracker").style.display = 'none';
 	document.getElementById("cerrar").style.display = 'none';
 
 	let dia = actualizarHora();
@@ -12,9 +13,12 @@ function butHoy(){
 }
 
 function butAcumulado(){
+
+	
 	document.getElementById("hoy").style.display = 'none';
 	document.getElementById("actividad").style.display = 'none';
 	document.getElementById("acumulado").style.display = 'block';
+	document.getElementById("tracker").style.display = 'none';
 	document.getElementById("cerrar").style.display = 'none';
 }
 
@@ -22,13 +26,36 @@ function butCerrar(){
 	document.getElementById("hoy").style.display = 'none';
 	document.getElementById("actividad").style.display = 'none';
 	document.getElementById("acumulado").style.display = 'none';
+	document.getElementById("tracker").style.display = 'none';
 	document.getElementById("cerrar").style.display = 'block';
 }
 
+function butTracker(){
+	document.getElementById("hoy").style.display = 'none';
+	document.getElementById("actividad").style.display = 'none';
+	document.getElementById("acumulado").style.display = 'none';
+	document.getElementById("tracker").style.display = 'block';
+	document.getElementById("cerrar").style.display = 'none';
+}
+
+let ultimaActividad = undefined;
+
+function atras(){
+	mostrarActividad(ultimaActividad);
+}
+
 function mostrarActividad(id){
+	ultimaActividad = id;
 	document.getElementById("hoy").style.display = 'none';
 	document.getElementById("actividad").style.display = 'block';
 	document.getElementById("acumulado").style.display = 'none';
+	document.getElementById("tracker").style.display = 'none';
 	document.getElementById("cerrar").style.display = 'none';
 	inicializarVistaActividad(id);
+	let ventanas=document.getElementsByClassName("cards-container")
+	for (let i = 0; i < ventanas.length; i++) {
+	  ventanas[i].scrollTo(0, 0);
+	}
+	
 }
+
